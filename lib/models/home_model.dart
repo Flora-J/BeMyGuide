94363f36117a7ba2c8ebfbfbfd78b7eba42e6cc2
Flora <flora.julie88@gmail.com>
2022-11-29 09:52:24 +0100

    Skeleton Project

---
+++ lib/models/home_model.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeModel extends StatefulWidget {
+  const HomeModel({Key? key}) : super(key: key);
+
+  @override
+  State<HomeModel> createState() => _HomeModelState();
+}
+
+class _HomeModelState extends State<HomeModel> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
